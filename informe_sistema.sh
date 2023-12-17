7a8d4c27acb01dd50ad3aa3a3fa03cc53cdd6ec7
cduarte <duarte.camd@gmail.com>
2023-12-17 19:47:39 -0300

    Obtengo e imprimo el hostname.

--- informe_sistema.sh
+++ informe_sistema.sh
@@ -9,11 +9,13 @@ echo "Iniciando informe del sistema."
 # Obtengo los datos.
 FECHA=$(date "+%d-%m-%Y %H:%M:%S %Z")
 USUARIO=$(whoami)
+HOSTNAME=$(hostname)
 
 # Imprimo el informe.
 echo
 echo "Fecha: ${FECHA}"
 echo "Usuario: ${USUARIO}"
+echo "Hostname: ${HOSTNAME}"
 echo
 
 echo "FIN."
