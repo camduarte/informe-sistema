eaeeaa5f38d0bde1dd7239ac14340e778b2013e7
cduarte <duarte.camd@gmail.com>
2023-12-17 19:28:44 -0300

    Agrego fecha.

--- informe_sistema.sh
+++ informe_sistema.sh
@@ -7,10 +7,14 @@
 echo "Iniciando informe del sistema."
 
 # Obtengo los datos.
+FECHA=$(date "+%d-%m-%Y %H:%M:%S %Z")
 USUARIO=$(whoami)
 
 # Imprimo el informe.
+echo
+echo "Fecha: ${FECHA}"
 echo "Usuario: ${USUARIO}"
+echo
 
 echo "FIN."
 exit 0
