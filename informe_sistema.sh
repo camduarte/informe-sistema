72aa8c85bed5f5a5962ed4e345ff6aff99a00d15
cduarte <duarte.camd@gmail.com>
2023-12-18 17:50:59 -0300

    Agrego informacón del kernel.

--- informe_sistema.sh
+++ informe_sistema.sh
@@ -10,12 +10,14 @@ echo "Iniciando informe del sistema."
 FECHA=$(date "+%d-%m-%Y %H:%M:%S %Z")
 USUARIO=$(whoami)
 HOSTNAME=$(hostname)
+KERNEL=$(uname -r)
 
 # Imprimo el informe.
 echo
 echo "Fecha: ${FECHA}"
 echo "Usuario: ${USUARIO}"
 echo "Hostname: ${HOSTNAME}"
+echo "Kernel: ${KERNEL}"
 echo
 
 echo "FIN."
